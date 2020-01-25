c139974efe930f32c0287731e92cb05afcb9f128
Evan Smith <smith.m.evan@gmail.com>
2020-01-25 11:49:25 +0000

    changed web parent and nav to bring in nav to web parent

--- client/src/components/WebParent.vue
+++ client/src/components/WebParent.vue
@@ -1,15 +1,18 @@
 <template lang="html">
   <div id="web-parent">
-    <h1>{{ article }}</h1>
+    <!-- <h1>{{ article }}</h1>
     <ul>
       <li v-for="item in articles"> {{ item }}</li>
-    </ul>
+    </ul> -->
+    <nav></nav>
+    <select-article-form :articles="articles"></select-article-form> 
   </div>
 
 </template>
 
 <script>
 import fetch_assistant from '../services/fetch_assistant'
+import Nav from './components/Nav.vue'
 export default {
   name: "web-parent",
   data () {
@@ -24,6 +27,9 @@ export default {
 
       fetch_assistant.getArticle("https://content.guardianapis.com/business/2020/jan/24/greta-thunberg-davos-leaders-ignored-climate-activists-demands")
         .then(res => this.article = res)
+  },
+  components: {
+    'nav': Nav
   }
 }
 </script>
