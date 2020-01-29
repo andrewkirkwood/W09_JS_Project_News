207e2b13d7b678a7bb5d0e21d157aed21ec697d4
Stephen Ramsay <rabrandom@gmail.com>
2020-01-29 10:11:08 +0000

    updated seeds

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,43 +3,107 @@ db.dropDatabase();
 
 db.articles.insertMany([
   {
-    sectionId: "business",
-    webTitle: "Greta Thunberg: Davos leaders ignored climate activists’ demands",
-    apiUrl: "https://content.guardianapis.com/business/2020/jan/24/greta-thunberg-davos-leaders-ignored-climate-activists-demands"
+    "id": "technology/2020/jan/28/boris-johnson-gives-green-light-for-huawei-5g-infrastructure-role",
+    "type": "article",
+    "sectionId": "technology",
+    "sectionName": "Technology",
+    "webPublicationDate": "2020-01-28T15:56:03Z",
+    "webTitle": "Raab defends green light for Huawei 5G infrastructure role",
+    "webUrl": "https://www.theguardian.com/technology/2020/jan/28/boris-johnson-gives-green-light-for-huawei-5g-infrastructure-role",
+    "apiUrl": "https://content.guardianapis.com/technology/2020/jan/28/boris-johnson-gives-green-light-for-huawei-5g-infrastructure-role",
+    "isHosted": false,
+    "pillarId": "pillar/news",
+    "pillarName": "News"
   },
   {
-    sectionId: "sport",
-    webTitle: "South Africa v England: rain delays start of fourth Test – live!",
-    apiUrl: "https://content.guardianapis.com/sport/live/2020/jan/24/south-africa-v-england-fourth-test-day-one-live"
+    "id": "technology/2020/jan/28/huawei-decision-takes-uk-down-path-of-least-resistance",
+    "type": "article",
+    "sectionId": "technology",
+    "sectionName": "Technology",
+    "webPublicationDate": "2020-01-28T13:10:48Z",
+    "webTitle": "Huawei decision takes UK down path of least resistance",
+    "webUrl": "https://www.theguardian.com/technology/2020/jan/28/huawei-decision-takes-uk-down-path-of-least-resistance",
+    "apiUrl": "https://content.guardianapis.com/technology/2020/jan/28/huawei-decision-takes-uk-down-path-of-least-resistance",
+    "isHosted": false,
+    "pillarId": "pillar/news",
+    "pillarName": "News"
   },
   {
-    sectionId: "mindofbaz",
-    webTitle: "the french person converts french to german, the spanish person converts german to spanish... this is a bit weird because they both speak object",
-    apiUrl: "https://themindofbaz.co.uk/public/images/languages.png"
+    "id": "technology/2020/jan/28/huawei-decision-is-a-sensible-compromise-but-could-still-anger-us",
+    "type": "article",
+    "sectionId": "technology",
+    "sectionName": "Technology",
+    "webPublicationDate": "2020-01-28T13:06:54Z",
+    "webTitle": "Huawei decision is a sensible compromise but could still anger US",
+    "webUrl": "https://www.theguardian.com/technology/2020/jan/28/huawei-decision-is-a-sensible-compromise-but-could-still-anger-us",
+    "apiUrl": "https://content.guardianapis.com/technology/2020/jan/28/huawei-decision-is-a-sensible-compromise-but-could-still-anger-us",
+    "isHosted": false,
+    "pillarId": "pillar/news",
+    "pillarName": "News"
   },
   {
-    sectionId: "artanddesign",
-    webTitle: "George Shaw’s My Baby Is Alright … from teenage kicks to lonely men",
-    apiUrl: "https://content.guardianapis.com/artanddesign/2020/jan/24/george-shaw-my-baby-is-alright-she-doesnt-mind-a-bit-of-dirt"
+    "id": "technology/2020/jan/28/boris-johnson-gets-final-warning-with-huawei-5g-verdict-imminent",
+    "type": "article",
+    "sectionId": "technology",
+    "sectionName": "Technology",
+    "webPublicationDate": "2020-01-28T09:49:52Z",
+    "webTitle": "Boris Johnson gets final warning with Huawei 5G verdict imminent",
+    "webUrl": "https://www.theguardian.com/technology/2020/jan/28/boris-johnson-gets-final-warning-with-huawei-5g-verdict-imminent",
+    "apiUrl": "https://content.guardianapis.com/technology/2020/jan/28/boris-johnson-gets-final-warning-with-huawei-5g-verdict-imminent",
+    "isHosted": false,
+    "pillarId": "pillar/news",
+    "pillarName": "News"
   },
   {
-    sectionId: "music",
-    webTitle: "Okay Kaya: Watch This Liquid Pour Itself review",
-    apiUrl: "https://content.guardianapis.com/music/2020/jan/24/okay-kaya-watch-this-liquid-pour-itself-review"
+    "id": "technology/2020/jan/27/boris-johnson-hints-compromise-huawei-5g",
+    "type": "article",
+    "sectionId": "technology",
+    "sectionName": "Technology",
+    "webPublicationDate": "2020-01-27T17:07:54Z",
+    "webTitle": "Boris Johnson hints at compromise over Huawei and 5G",
+    "webUrl": "https://www.theguardian.com/technology/2020/jan/27/boris-johnson-hints-compromise-huawei-5g",
+    "apiUrl": "https://content.guardianapis.com/technology/2020/jan/27/boris-johnson-hints-compromise-huawei-5g",
+    "isHosted": false,
+    "pillarId": "pillar/news",
+    "pillarName": "News"
   },
   {
-    sectionId: "sport",
-    webTitle: "F1’s new-season storylines are tantalising, but pay TV is limiting potential",
-    apiUrl: "https://content.guardianapis.com/sport/blog/2020/jan/24/formula-one-lewis-hamilton-max-verstappen-charles-leclerc-storylines-pay-tv-limit-potential"
+    "id": "technology/2020/jan/27/huawei-decision-weighs-technological-benefits-and-political-risks",
+    "type": "article",
+    "sectionId": "technology",
+    "sectionName": "Technology",
+    "webPublicationDate": "2020-01-27T15:58:37Z",
+    "webTitle": "Huawei decision weighs technological benefits against political risks",
+    "webUrl": "https://www.theguardian.com/technology/2020/jan/27/huawei-decision-weighs-technological-benefits-and-political-risks",
+    "apiUrl": "https://content.guardianapis.com/technology/2020/jan/27/huawei-decision-weighs-technological-benefits-and-political-risks",
+    "isHosted": false,
+    "pillarId": "pillar/news",
+    "pillarName": "News"
   },
   {
-    sectionId: "books",
-    webTitle: "Wild Game by Adrienne Brodeur review – the reader wants to scream",
-    apiUrl: "https://content.guardianapis.com/books/2020/jan/24/wild-game-adrienne-brodeur-review"
+    "id": "technology/2020/jan/27/uk-sovereignty-in-jeopardy-if-huawei-used-for-5g-us-warns",
+    "type": "article",
+    "sectionId": "technology",
+    "sectionName": "Technology",
+    "webPublicationDate": "2020-01-27T00:31:57Z",
+    "webTitle": "UK sovereignty in jeopardy if Huawei used for 5G, US warns",
+    "webUrl": "https://www.theguardian.com/technology/2020/jan/27/uk-sovereignty-in-jeopardy-if-huawei-used-for-5g-us-warns",
+    "apiUrl": "https://content.guardianapis.com/technology/2020/jan/27/uk-sovereignty-in-jeopardy-if-huawei-used-for-5g-us-warns",
+    "isHosted": false,
+    "pillarId": "pillar/news",
+    "pillarName": "News"
   },
   {
-    sectionId: "liveblog",
-    webTitle: "Davos 2020: Greta Thunberg says climate demands have been 'completely ignored' - live",
-    apiUrl: "https://content.guardianapis.com/business/live/2020/jan/24/lagarde-georgieva-thunberg-mnuchin-davos-markets-uk-pmi-business-live"
-  },
+    "id": "technology/2020/jan/26/the-five-factors-that-affect-early-grey-hair-stress-hormones-genetics-heredity-smoking-cancer-drugs",
+    "type": "article",
+    "sectionId": "technology",
+    "sectionName": "Technology",
+    "webPublicationDate": "2020-01-26T05:30:15Z",
+    "webTitle": "The five: factors that affect early greying",
+    "webUrl": "https://www.theguardian.com/technology/2020/jan/26/the-five-factors-that-affect-early-grey-hair-stress-hormones-genetics-heredity-smoking-cancer-drugs",
+    "apiUrl": "https://content.guardianapis.com/technology/2020/jan/26/the-five-factors-that-affect-early-grey-hair-stress-hormones-genetics-heredity-smoking-cancer-drugs",
+    "isHosted": false,
+    "pillarId": "pillar/news",
+    "pillarName": "News"
+  }
 ]);
